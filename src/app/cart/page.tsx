4610863b761da455274bcaeee3632a1ca033b270
maryamaliii <162467024+maryamaliii@users.noreply.github.com>
2025-01-14 01:22:09 +0500

    Create template 09 sanity

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -18,8 +18,8 @@ const CartPage = () => {
     .toFixed(2);
 
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold mb-6">Cart</h1>
+    <div className="p-4 sm:p-8">
+      <h1 className="text-2xl sm:text-3xl font-bold mb-6">Cart</h1>
       {cart.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
@@ -27,9 +27,9 @@ const CartPage = () => {
           {cart.map((item) => (
             <div
               key={item.id}
-              className="flex items-center justify-between border p-4 rounded-lg shadow"
+              className="flex flex-col sm:flex-row items-center justify-between border p-4 rounded-lg shadow"
             >
-              <div className="flex items-center space-x-4">
+              <div className="flex items-center space-x-4 mb-4 sm:mb-0">
                 {/* Display product image */}
                 {item.image && (
                   <Image
@@ -49,42 +49,40 @@ const CartPage = () => {
                   </p>
                 </div>
               </div>
-              <div className="flex items-center space-x-2">
+              <div className="flex flex-row gap-4 sm:flex-row items-center text-lg space-y-2 sm:space-y-0 sm:space-x-2">
                 <button
                   onClick={() => updateQuantity(item.id, item.quantity - 1)}
-                  className="px-4 py-2 bg-white text-black text-lg rounded border border-gray-300 hover:bg-gray-100 transition"
+                  className="px-3 py-1 sm:px-4 sm:py-2 bg-white text-black text-sm sm:text-lg rounded border border-gray-300 hover:bg-gray-100 transition"
                   disabled={item.quantity <= 1} // Disable the button if quantity is 1 or less
                 >
                   -
                 </button>
                 <button
                   onClick={() => updateQuantity(item.id, item.quantity + 1)}
-                  className="px-4 py-2 bg-white text-black text-lg rounded border border-gray-300 hover:bg-gray-100 transition"
+                  className="px-3 py-1 sm:px-4 sm:py-2 bg-white text-black text-sm sm:text-lg rounded border border-gray-300 hover:bg-gray-100 transition"
                 >
                   +
                 </button>
                 <button
                   onClick={() => removeFromCart(item.id)}
-                  className="px-4 py-2 bg-orange-500 text-white rounded hover:bg-orange-700 transition"
+                  className="px-3 py-1 sm:px-4 sm:py-2 bg-orange-500 text-white text-sm sm:text-base rounded hover:bg-orange-700 transition"
                 >
                   Remove
                 </button>
               </div>
             </div>
           ))}
-         
+
           {/* Grand Total Section */}
           <div className="mt-6 text-left">
-            <p className="text-xl font-semibold">
-             Total: ${grandTotal}
-            </p>
+            <p className="text-xl font-semibold">Total: ${grandTotal}</p>
           </div>
           {/* Checkout Button */}
           <div className="mt-6 text-right">
             <Link href="/generate-tracking">
               <button
                 disabled={cart.length === 0}
-                className={`px-6 py-2 bg-orange-500 text-white rounded hover:bg-orange-600 transition ${
+                className={`px-4 py-2 sm:px-6 sm:py-2 bg-orange-500 text-white rounded hover:bg-orange-600 transition ${
                   cart.length === 0 ? "opacity-50 cursor-not-allowed" : ""
                 }`}
               >
